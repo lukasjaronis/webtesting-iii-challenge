ca6145ee4c517f3d62badc588f994bfdadc1d5f4
lukasjaronis <jaronisluke13@gmail.com>
2019-12-16 17:10:53 -0500

    locked and closed test on display.test.js

---
+++ src/tests/display.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { closed, locked } from '../controls/Controls';
+
+import Display from '../display/Display';
+
+// closed
+test('Matches closed gate snapshot', () => {
+const closedClass = `led ${closed ? 'red-led' : 'green-led'}`;
+const tree = render(<Display className={closedClass} />)
+expect(tree.baseElement).toMatchSnapshot();
+})
+
+// locked
+test('Matches locked gate snapshot', () => {
+    const lockedClass = `led ${locked ? 'red-led' : 'green-led'}`;
+    const tree = render(<Display className={lockedClass} />)
+    expect(tree.baseElement).toMatchSnapshot(); 
+})
