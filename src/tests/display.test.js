ad4b18b0c858a612e20c9eea7a94dc81e8ac250b
lukasjaronis <jaronisluke13@gmail.com>
2019-12-16 17:27:31 -0500

    open/unlocked test. open/closed and locked/unlocked test

--- src/tests/display.test.js
+++ src/tests/display.test.js
@@ -18,3 +18,26 @@ test('Matches locked gate snapshot', () => {
     expect(tree.baseElement).toMatchSnapshot(); 
 })
 
+test('unlocked and open default', () => {
+    const defaultState = {
+        closed: false,
+        locked: false
+    }
+
+    const { getByText } = render(<Display closed={defaultState.closed} locked={defaultState.locked} />)
+
+    expect(getByText(/unlocked/i)).toBeDefined();
+    expect(getByText(/open/i)).toBeDefined();
+})
+
+test('displays if gate is open/closed and locked/unlocked', () => {
+    const defaultState = {
+        closed: true,
+        locked: true
+    }
+
+    const { getByText } = render(<Display closed={defaultState.closed} locked={defaultState.locked} />)
+
+    expect(getByText(defaultState.locked ? /locked/i : /unlocked/i)).toBeDefined();
+    expect(getByText(defaultState.closed ? /closed/i : /open/i)).toBeDefined();
+})
