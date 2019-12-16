b7d0b45578492c420c93c81d730ecd4b493c4d88
lukasjaronis <jaronisluke13@gmail.com>
2019-12-16 18:13:50 -0500

    gate cannot be closed or open when gate is locked

--- src/tests/controls.test.js
+++ src/tests/controls.test.js
@@ -17,3 +17,14 @@ test('Matches snapshot, fireevent on button lock gate', () => {
     fireEvent.click(button);
     expect(baseElement).toMatchSnapshot(); 
 })
+
+test('gate cannot be closed or opened when gate is locked', () => {
+    const toggle = jest.fn();
+    const { getByText, baseElement } = render(<Controls locked={true} closed={true} toggle={toggle} />)
+
+    const openGate = getByText(/open gate/i)
+    fireEvent.click(openGate);
+    expect(toggle).not.toHaveBeenCalled();
+    expect(baseElement).toMatchSnapshot();
+
+})
